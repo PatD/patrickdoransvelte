385d38a76e475c6ca22dd77b67f1c5d535909f81
patd <pdoran@gmail.com>
2020-11-11 09:34:55 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+import { onMount } from 'svelte';
+
+let items = [];
+
+onMount(async () => {
+	const res = await fetch(`https://patrickdoran.com/headless/wp-json/wp/v2/posts`);
+	items = await res.json();
+});
+</script>
+
+{#each items as listitem}
+<li>
+	{@html listitem.content.rendered}
+</li>
+{:else}
+<p>Loading data from headless WordPress into a Svelte component...</p>
+{/each}
