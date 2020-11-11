30e0e4e621d106a19f8c7207a03554f46be41ac7
patd <pdoran@gmail.com>
2020-11-11 10:46:43 -0500

    In progress formatting Wordpress JSON response

--- src/App.svelte
+++ src/App.svelte
@@ -3,15 +3,39 @@ import { onMount } from 'svelte';
 let wordpressUrl = 'https://patrickdoran.com/headless/wp-json/wp/v2/posts';
 let items = [];
 
+function filterResults(loadedResults){
+	// 
+	console.log(loadedResults)
+
+	loadedResults.forEach((element, index, array) => {
+		
+		console.log(element.content.rendered); // strip html here
+		console.log(element.title.rendered); // 100, 200, 300
+		console.log(element.excerpt.rendered); // strip html here
+		console.log(element.date); // 100, 200, 300
+		console.log(element.id); // 100, 200, 300
+		
+		console.log(index); // 0, 1, 2
+		console.log(array); // same myArray object 3 times
+	})
+}
+
+
 onMount(async () => {
 	const res = await fetch(wordpressUrl);
-	items = await res.json();
+	// loop results, strip markup
+
+	// items = await res.json()
+
+	let loadedResults = await res.json();
+	filterResults(loadedResults)
+
 });
 </script>
 
 {#each items as listitem}
 <li>
-	{@html listitem.content.rendered}
+	{listitem.content.rendered} <a href={listitem.excerpt.rendered}>{listitem.title.rendered}</a> {listitem.date}
 </li>
 {:else}
 <p>Loading data from headless WordPress into a Svelte component...</p>
