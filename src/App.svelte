615e7a52a52e52e7786a80e91be5ef4c21be5047
patd <pdoran@gmail.com>
2020-11-12 22:34:47 -0500

    Add array.map to strip markup from Wordpress GraphQL response

--- src/App.svelte
+++ src/App.svelte
@@ -1,42 +1,56 @@
 <script>
 import { onMount } from 'svelte';
-let wordpressUrl = 'https://patrickdoran.com/headless/wp-json/wp/v2/posts';
+let wordPressGraphQL = 'https://patrickdoran.com/headless/graphql'
 let items = [];
+let filteredItems = [];
 
-function filterResults(loadedResults){
-	// 
-	console.log(loadedResults)
-
-	loadedResults.forEach((element, index, array) => {
-		
-		console.log(element.content.rendered); // strip html here
-		console.log(element.title.rendered); // 100, 200, 300
-		console.log(element.excerpt.rendered); // strip html here
-		console.log(element.date); // 100, 200, 300
-		console.log(element.id); // 100, 200, 300
-		
-		console.log(index); // 0, 1, 2
-		console.log(array); // same myArray object 3 times
-	})
-}
 
 
 onMount(async () => {
-	const res = await fetch(wordpressUrl);
-	// loop results, strip markup
+// Queries Headless WordPress site configured for GraphQL
+	const res = await fetch(wordPressGraphQL, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			query: `
+				{
+				posts {
+					edges {
+					node {
+						excerpt
+						date
+						postId
+						title
+						content
+					}
+					}
+				}
+				}
+			`,
+		}),
+	})
 
-	// items = await res.json()
+	let graphQLresults = await res.json()
 
-	let loadedResults = await res.json();
-	filterResults(loadedResults)
+	items = graphQLresults.data.posts.edges;
+
+	// Remove Wordpress markup from GraphQL 
+	let mappedItems = items.map(x => {
+		x.node.excerpt = x.node.excerpt.replace( /(<([^>]+)>)/ig, '');
+		x.node.content = x.node.content.replace( /(<([^>]+)>)/ig, '');
+		// console.log(x.node)
+		filteredItems.push(x.node)
+	})
 
 });
 </script>
 
 {#each items as listitem}
 <li>
-	{listitem.content.rendered} <a href={listitem.excerpt.rendered}>{listitem.title.rendered}</a> {listitem.date}
+	{listitem.node.content} <a href={listitem.node.excerpt} target='_blank' rel='noreferrer'>{listitem.node.title}</a> {listitem.node.date}
 </li>
 {:else}
-<p>Loading data from headless WordPress into a Svelte component...</p>
+<li>Loading GraphQL data from a headless WordPress site into a Svelte component...</li>
 {/each}
